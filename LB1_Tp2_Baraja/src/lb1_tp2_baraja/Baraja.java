c4940092cf61fb41ec9117c6dc427a8e3d3662f8
matiSqui <matisqui@gmail.com>
2024-08-28 17:05:19 -0300

    Correcciones y creaciones
    Creación del mazo de cartas españolas

--- LB1_Tp2_Baraja/src/lb1_tp2_baraja/Baraja.java
+++ LB1_Tp2_Baraja/src/lb1_tp2_baraja/Baraja.java
@@ -6,11 +6,46 @@ import java.util.Collections;
 public class Baraja {
 
     public Baraja() {
+
     }
 
-    ArrayList<Carta> cartas = new ArrayList<>();
+    @Override
+    public String toString() {
+        return "Baraja{" +
+                "cartas=" + cartas +
+                '}' + "----------------";
+    }
+
+
+    static ArrayList<Carta> cartas = new ArrayList<>();
 
     //Metodos
+
+    public void crearMazoEspanol(){
+        String[] palos = {"Oros", "Copas", "Espadas", "Bastos"};
+
+        for (String palo : palos) {
+            for (int valor = 1; valor <=7 ; valor++) {
+                Carta carta = new Carta(valor, palo);
+                cartas.add(carta);
+            }
+
+            for (int valor = 10; valor <=12 ; valor++) {
+                Carta carta = new Carta(valor, palo);
+                cartas.add(carta);
+            }
+        }
+    }
+
+    public void mostrarMazo() {
+
+        for (Carta carta : cartas) {
+            System.out.println(carta.toString());
+
+        }
+
+    }
+
     public void agregarCartas(Carta carta){
 
         cartas.add(carta);
@@ -20,8 +55,14 @@ public class Baraja {
 
         Collections.shuffle(cartas);
 
-        System.out.println("Luego de barajar: " + cartas);
-        
+        System.out.println("Luego de barajar");
+        for (Carta carta : cartas) {
+
+            System.out.println(carta.toString());
+        }
+
     }
+
     
+
 }
--- LB1_Tp2_Baraja/src/lb1_tp2_baraja/LB1_Tp2_Baraja.java
+++ LB1_Tp2_Baraja/src/lb1_tp2_baraja/LB1_Tp2_Baraja.java
@@ -1,9 +1,23 @@
 package lb1_tp2_baraja;
 
+
+
 public class LB1_Tp2_Baraja {
 
     public static void main(String[] args) {
-        
+
+
+        Baraja baraja = new Baraja();
+
+        baraja.crearMazoEspanol();
+        baraja.mostrarMazo();
+
+        System.out.println("----------------------");
+        baraja.barajar();
+
+
+
+
+
     }
-    
 }
