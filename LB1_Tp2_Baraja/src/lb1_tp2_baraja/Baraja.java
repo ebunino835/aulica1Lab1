1dacbfaffed2f57e95ebb22b899cd1d165e03881
matiSqui <88115710+sjelx@users.noreply.github.com>
2024-08-28 15:35:02 -0300

    Update Baraja.java
    Segui con el metodo barajar

--- LB1_Tp2_Baraja/src/lb1_tp2_baraja/Baraja.java
+++ LB1_Tp2_Baraja/src/lb1_tp2_baraja/Baraja.java
@@ -1,6 +1,7 @@
 package lb1_tp2_baraja;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Baraja {
 
@@ -9,9 +10,7 @@ public class Baraja {
 
     ArrayList<Carta> cartas = new ArrayList<>();
 
-
     //Metodos
-
     public static void agregarCartas(Carta carta){
 
         cartas.add(carta)
@@ -19,7 +18,9 @@ public class Baraja {
 
     public static void barajar(ArrayList cartas){
 
-        
+        Collections.shuffle(cartas);
+
+        system.out.println("Luego de barajar: " + cartas);
         
     }
     
